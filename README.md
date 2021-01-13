e74666400dd373f663221b4855be44849bfd9e7d
VVugo <adrien.brest01@gmail.com>
2021-01-13 13:40:28 +0100

    WIN:Architecture du projet

---
+++ README.md
@@ -0,0 +1,116 @@
+# Services web communication
+
+- Réalisation d'une application client-serveur de recherche client sous **PHP** en PDO en ayant une architecture SOAP ou REST.
+- Base de donnée sous **MySqlServer**.
+---
+
+## Utilisation de Composer
+
+L'utilisation de composer dans ce projet permet d'utiliser des dépendances dans notre projet, on utilisera les dépendances suivantes.
+
+##### .env :
+
+- Défini un fichier de variables d'environnements dans lequelles on enregistrera les accès à notre base de donnée.
+
+##### PhpStan :
+- Outil d'analyse statique, il détecte les problèmes de structure dans le code.
+
+##### PHPMD / PHPCS :
+- Dépendance permettant de détecter les sources possibles de bugs provenant de méthodes / expressions trop complexes.
+- Garantit le respect des conventions de développement PSR.
+
+##### Prettier :
+- Formate le code de façon automatique afin de corriger une partie des problèmes remontés par PHPCS.
+
+##### Bootstrap :
+- La mise en forme de notre application web
+
+---
+## Installation Composer
+Intaller composer dans le répertoire actuel, executé les lignes de commandes suivantes dans votre terminal afin d'installer automatiquement Composer ou suivez le guide d'[installation](https://getcomposer.org/download/).
+
+**Installation** :
+```php
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === '795f976fe0ebd8b75f26a6dd68f78fd3453ce79f32ecb33e7fd087d39bfeb978342fb73ac986cd4f54edd0dc902601dc') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+```
+
+**Initialisation** :
+
+```php
+php composer init
+```
+----
+
+## Installation des dépendances via composer
+---
+**Installation | .ENV** :
+```php
+php composer require symfony/dotenv
+```
+**Utilisation | .ENV** :
+```php
+use SymfonyComponentDotenvDotenv;
+
+$dotenv = new Dotenv();
+$dotenv->load(__DIR__.'/.env');
+
+// you can also load several files
+$dotenv->load(__DIR__.'/.env', __DIR__.'/.env.dev');
+
+// overwrites existing env variables
+$dotenv->overload(__DIR__.'/.env');
+
+// loads .env, .env.local, and .env.$APP_ENV.local or .env.$APP_ENV
+$dotenv->loadEnv(__DIR__.'/.env');
+```
+---
+**Installation | PHPSTAN** :
+```php
+php composer require --dev phpstan/phpstan
+```
+---
+**Installation | PHPMD** :
+```php
+php composer require phpmd/phpmd
+```
+---
+**Installation | PHPCS** :
+```php
+php composer require squizlabs/php_codesniffer
+```
+---
+**Installation | PRETTIER** :
+```npm
+npm install --save-dev prettier @plugin-php
+```
+---
+**Installation GIT | NPM** :
+```php
+git init
+git remote add origin https://github.com/VVugo/ServicesWebCommunications.git
+
+npm init
+```
+---
+## Installation de Husky
+On utilise Husky dans ce projet afin de créer un hooks (un script qui s'éxécutera avant un commit dans notre projet).
+```
+npm insall husky --save-dev
+```
+
+**Modification de notre fichier package.json :**
+Dans package.json il est important d'ajouter les lignes suivantes afin de pouvoir utiliser nos dépendances automatiquement a lancement d'un pre-commit 
+```
+   "husky": {
+        "hooks": {
+            "pre-commit": "vendor/bin/phpcs src/ && vendor/bin/phpcbf src/ && vendor/bin/phpstan src/ && vendor/bin/phpmd src/ text cleancode"
+        }
+    },
+
+```
+
+## Edited By
+**Auteur :**
---
+++ vendor/composer/InstalledVersions.php
@@ -0,0 +1,384 @@
+<?php
+
+
+
+
+
+
+
+
+
+
+
+namespace Composer;
+
+use Composer\Semver\VersionParser;
+
+
+
+
+
+
+class InstalledVersions
+{
+private static $installed = array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'serviceweb/communication',
+  ),
+  'versions' => 
+  array (
+    'composer/xdebug-handler' => 
+    array (
+      'pretty_version' => '1.4.5',
+      'version' => '1.4.5.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f28d44c286812c714741478d968104c5e604a1d4',
+    ),
+    'pdepend/pdepend' => 
+    array (
+      'pretty_version' => '2.8.0',
+      'version' => '2.8.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c64472f8e76ca858c79ad9a4cf1e2734b3f8cc38',
+    ),
+    'phpmd/phpmd' => 
+    array (
+      'pretty_version' => '2.9.1',
+      'version' => '2.9.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ce10831d4ddc2686c1348a98069771dd314534a8',
+    ),
+    'phpstan/phpstan' => 
+    array (
+      'pretty_version' => '0.12.66',
+      'version' => '0.12.66.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4110a2425c6bd53acbdfcda07885e87b66e9ba3e',
+    ),
+    'psr/container' => 
+    array (
+      'pretty_version' => '1.0.0',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b7ce3b176482dbbc1245ebf52b181af44c2cf55f',
+    ),
+    'psr/container-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '1.0',
+      ),
+    ),
+    'psr/log' => 
+    array (
+      'pretty_version' => '1.1.3',
+      'version' => '1.1.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0f73288fd15629204f9d42b7055f72dacbe811fc',
+    ),
+    'serviceweb/communication' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'squizlabs/php_codesniffer' => 
+    array (
+      'pretty_version' => '3.5.8',
+      'version' => '3.5.8.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9d583721a7157ee997f235f327de038e7ea6dac4',
+    ),
+    'symfony/config' => 
+    array (
+      'pretty_version' => 'v5.2.1',
+      'version' => '5.2.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd0a82d965296083fe463d655a3644cbe49cbaa80',
+    ),
+    'symfony/dependency-injection' => 
+    array (
+      'pretty_version' => 'v5.2.1',
+      'version' => '5.2.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '7f8a9e9eff0581a33e20f6c5d41096fe22832d25',
+    ),
+    'symfony/deprecation-contracts' => 
+    array (
+      'pretty_version' => 'v2.2.0',
+      'version' => '2.2.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5fa56b4074d1ae755beb55617ddafe6f5d78f665',
+    ),
+    'symfony/dotenv' => 
+    array (
+      'pretty_version' => 'v5.2.1',
+      'version' => '5.2.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '204a9dc6f70a13d9d24ebbf2c5ce51be235f3d7b',
+    ),
+    'symfony/filesystem' => 
+    array (
+      'pretty_version' => 'v5.2.1',
+      'version' => '5.2.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'fa8f8cab6b65e2d99a118e082935344c5ba8c60d',
+    ),
+    'symfony/polyfill-ctype' => 
+    array (
+      'pretty_version' => 'v1.22.0',
+      'version' => '1.22.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c6c942b1ac76c82448322025e084cadc56048b4e',
+    ),
+    'symfony/polyfill-php80' => 
+    array (
+      'pretty_version' => 'v1.22.0',
+      'version' => '1.22.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'dc3063ba22c2a1fd2f45ed856374d79114998f91',
+    ),
+    'symfony/service-contracts' => 
+    array (
+      'pretty_version' => 'v2.2.0',
+      'version' => '2.2.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd15da7ba4957ffb8f1747218be9e1a121fd298a1',
+    ),
+    'symfony/service-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '1.0',
+      ),
+    ),
+    'twbs/bootstrap' => 
+    array (
+      'pretty_version' => 'v4.5.3',
+      'version' => '4.5.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a716fb03f965dc0846df479e14388b1b4b93d7ce',
+    ),
+    'twitter/bootstrap' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v4.5.3',
+      ),
+    ),
+  ),
+);
+
+
+
+
+
+
+
+public static function getInstalledPackages()
+{
+return array_keys(self::$installed['versions']);
+}
+
+
+
+
+
+
+
+
+
+public static function isInstalled($packageName)
+{
+return isset(self::$installed['versions'][$packageName]);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public static function satisfies(VersionParser $parser, $packageName, $constraint)
+{
+$constraint = $parser->parseConstraints($constraint);
+$provided = $parser->parseConstraints(self::getVersionRanges($packageName));
+
+return $provided->matches($constraint);
+}
+
+
+
+
+
+
+
+
+
+
+public static function getVersionRanges($packageName)
+{
+if (!isset(self::$installed['versions'][$packageName])) {
+throw new \OutOfBoundsException('Package "' . $packageName . '" is not installed');
+}
+
+$ranges = array();
+if (isset(self::$installed['versions'][$packageName]['pretty_version'])) {
+$ranges[] = self::$installed['versions'][$packageName]['pretty_version'];
+}
+if (array_key_exists('aliases', self::$installed['versions'][$packageName])) {
+$ranges = array_merge($ranges, self::$installed['versions'][$packageName]['aliases']);
+}
+if (array_key_exists('replaced', self::$installed['versions'][$packageName])) {
+$ranges = array_merge($ranges, self::$installed['versions'][$packageName]['replaced']);
+}
+if (array_key_exists('provided', self::$installed['versions'][$packageName])) {
+$ranges = array_merge($ranges, self::$installed['versions'][$packageName]['provided']);
+}
+
+return implode(' || ', $ranges);
+}
+
+
+
+
+
+public static function getVersion($packageName)
+{
+if (!isset(self::$installed['versions'][$packageName])) {
+throw new \OutOfBoundsException('Package "' . $packageName . '" is not installed');
+}
+
+if (!isset(self::$installed['versions'][$packageName]['version'])) {
+return null;
+}
+
+return self::$installed['versions'][$packageName]['version'];
+}
+
+
+
+
+
+public static function getPrettyVersion($packageName)
+{
+if (!isset(self::$installed['versions'][$packageName])) {
+throw new \OutOfBoundsException('Package "' . $packageName . '" is not installed');
+}
+
+if (!isset(self::$installed['versions'][$packageName]['pretty_version'])) {
+return null;
+}
+
+return self::$installed['versions'][$packageName]['pretty_version'];
+}
+
+
+
+
+
+public static function getReference($packageName)
+{
+if (!isset(self::$installed['versions'][$packageName])) {
+throw new \OutOfBoundsException('Package "' . $packageName . '" is not installed');
+}
+
+if (!isset(self::$installed['versions'][$packageName]['reference'])) {
+return null;
+}
+
+return self::$installed['versions'][$packageName]['reference'];
+}
+
+
+
+
+
+public static function getRootPackage()
+{
+return self::$installed['root'];
+}
+
+
+
+
+
+
+
+public static function getRawData()
+{
+return self::$installed;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public static function reload($data)
+{
+self::$installed = $data;
+}
+}
